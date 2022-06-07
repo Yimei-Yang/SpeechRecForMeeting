ee19c42184eacb03336d8c755e0c0561d6a7928f
Viv-Crowe <53358163+Viv-Crowe@users.noreply.github.com>
2022-06-07 13:16:20 -0400

    Minor changes

--- data and code/py files/main.py
+++ data and code/py files/main.py
@@ -1,5 +1,5 @@
 #preprocessing the data
-#import functions from data processing 
+#import functions from data processing
 import glob, os
 os.chdir("/content/drive/My Drive/Team 6/Signals-test")
 for file in glob.glob("*.wav"):
