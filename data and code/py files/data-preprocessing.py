d7b8f490cc590eadffc9adbaff8a15bc7b447011
Viv-Crowe <53358163+Viv-Crowe@users.noreply.github.com>
2022-06-07 13:11:16 -0400

    Added XML to pandas function, adapted from dataset_view notebook

--- data and code/py files/data-preprocessing.py
+++ data and code/py files/data-preprocessing.py
@@ -4,12 +4,17 @@ import contextlib
 import numpy as np
 import pandas as pd
 import re
+import xml.etree.ElementTree as et
+from pathlib import Path
 
 import glob, os
 import librosa
 
-#get a pd of [id, st_time, ed_time]
+
 def getInputSegmentTimes(list_of_timestamps, audio_file, segment_length, overlap_length):
+    '''
+    get a pd of [id, st_time, ed_time]
+    '''
     # generate meeting name
     meeting_id = audio_file.partition('.interaction.wav')[0]
     print(meeting_id)
@@ -33,7 +38,7 @@ def getInputSegmentTimes(list_of_timestamps, audio_file, segment_length, overlap
 
 def getInputSegments(audio_file, df_timestamps):
     '''
-    input: path to audio_file, df_timestamps
+    input: path to audio_file, df_timestamps['meeting_id','st_time','ed_time']
     output: [path/audio_chunk_{}, ..., path/audio_chunk_{}]
     '''
     audio = AudioSegment.from_wav(audio_file)
@@ -54,8 +59,11 @@ def getInputSegments(audio_file, df_timestamps):
 
     return segments
 
-#list of np_array(2d matrix)
+
 def getFeatures(output, np_list):
+  '''
+  get a list melspecs (i.e. a 2D np_array), one melspec per segment
+  '''
   for segment in output:
     signal,sr = librosa.load(segment,sr=None)
     if signal is None or len(signal) == 0:
@@ -65,12 +73,63 @@ def getFeatures(output, np_list):
       #save all np.arrays(.wav) files into an array -> X dataset
       np_list.append(melspect)
 
-def getLabels(segments, interruptions):
+def dialogueActsXMLtoPd(pathToDialogueActs):
+      """ main """
+    filenames =  glob.glob(pathToDialogueActs) 
+    filename = [] 
+    li = []
+    for filename in filenames :
+         
+         global left
+         parsed_xml = et.parse(filename)
+         #I hard-coded here but i think it would be easier to modify the panda dataframe later
+         dfcols = ['Id', 'st_time', 'ed_time', 'type', 'adjacency', 'original-type', 'channel', 'participant']
+         left = pd.DataFrame(columns=dfcols)
+
+         root = parsed_xml.getroot()
+         
+        
+         for diaAct in parsed_xml.findall('./dialogueact'):
+             uId = diaAct.get('{http://nite.sourceforge.net/}id')
+             sT = diaAct.get("starttime")
+             eT = diaAct.get("endtime")
+             tP = diaAct.get("type")
+             aJ = diaAct.get("adjacency")
+             oT = diaAct.get("original-type")
+             ch = diaAct.get("channel")
+             par = diaAct.get("participant")
+
+             
+             left = left.append(pd.Series([uId, sT, eT, tP, aJ, oT, ch, par], index=dfcols),ignore_index=True)
+             li.append(left)
+             
+    df = pd.concat(li, axis=0, ignore_index=True)
+    df.loc[:, 'st_time'] = pd.to_numeric(df.loc[:, 'st_time'])
+    df.loc[:, 'ed_time'] = pd.to_numeric(df.loc[:, 'ed_time'])
+    df = df.drop_duplicates(keep='first')
+
+    return df
+
+def addDAoIVariable(df):
+  # Add the 'Interruption' (bool) variable
+  df['DAoI'] = df['type'].str.contains('%-', regex = False)
+  df['DAoI'] = df['Interruption'].astype(bool)
+
+  # View what types are counted as Interruptions 
+  # print(df.loc[df['Interruption'], 'type'])
+  return df
+
+def getLabels(segments, df_interruptions):
+  '''
+  input: segments: a list of segment file names, df_interruptions['meeting_id','st_time','ed_time', 'daoi']
+  '''
     counts = np.empty(segments.shape[0])
 
     for seg_index, seg_row in segments.iterrows():
       for inter_index, inter_row in interruptions.iterrows():
-        if seg_row['st_time'] < inter_row['st_time'] and seg_row['ed_time'] > inter_row['ed_time']:
+        if seg_row['meeting_id'] != inter_row['meeting_id']:
+          continue
+        elif seg_row['st_time'] < inter_row['st_time'] and seg_row['ed_time'] > inter_row['ed_time']:
           counts[seg_index] += 1
         else:
           counts[seg_index] = 0
