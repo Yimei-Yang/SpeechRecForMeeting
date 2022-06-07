323f3ed47ffb973a99fd619dab7c62e04f2b2837
Viv-Crowe <53358163+Viv-Crowe@users.noreply.github.com>
2022-06-07 13:23:48 -0400

    Fixed indenting in data-preprocessing, imported functions to main

--- data and code/py files/data_preprocessing.py
+++ data and code/py files/data_preprocessing.py
@@ -74,41 +74,40 @@ def getFeatures(output, np_list):
       np_list.append(melspect)
 
 def dialogueActsXMLtoPd(pathToDialogueActs):
-      """ main """
-    filenames =  glob.glob(pathToDialogueActs) 
-    filename = [] 
-    li = []
-    for filename in filenames :
-         
-         global left
-         parsed_xml = et.parse(filename)
-         #I hard-coded here but i think it would be easier to modify the panda dataframe later
-         dfcols = ['Id', 'st_time', 'ed_time', 'type', 'adjacency', 'original-type', 'channel', 'participant']
-         left = pd.DataFrame(columns=dfcols)
+  """ main """
+  filenames = glob.glob(pathToDialogueActs)
+  filename = [] 
+  li = []
+  for filename in filenames :   
+    global left
+    parsed_xml = et.parse(filename)
+    #I hard-coded here but i think it would be easier to modify the panda dataframe later
+    dfcols = ['Id', 'st_time', 'ed_time', 'type', 'adjacency', 'original-type', 'channel', 'participant']
+    left = pd.DataFrame(columns=dfcols)
 
-         root = parsed_xml.getroot()
-         
-        
-         for diaAct in parsed_xml.findall('./dialogueact'):
-             uId = diaAct.get('{http://nite.sourceforge.net/}id')
-             sT = diaAct.get("starttime")
-             eT = diaAct.get("endtime")
-             tP = diaAct.get("type")
-             aJ = diaAct.get("adjacency")
-             oT = diaAct.get("original-type")
-             ch = diaAct.get("channel")
-             par = diaAct.get("participant")
+    root = parsed_xml.getroot()
+    
+  
+    for diaAct in parsed_xml.findall('./dialogueact'):
+      uId = diaAct.get('{http://nite.sourceforge.net/}id')
+      sT = diaAct.get("starttime")
+      eT = diaAct.get("endtime")
+      tP = diaAct.get("type")
+      aJ = diaAct.get("adjacency")
+      oT = diaAct.get("original-type")
+      ch = diaAct.get("channel")
+      par = diaAct.get("participant")
 
-             
-             left = left.append(pd.Series([uId, sT, eT, tP, aJ, oT, ch, par], index=dfcols),ignore_index=True)
-             li.append(left)
-             
-    df = pd.concat(li, axis=0, ignore_index=True)
-    df.loc[:, 'st_time'] = pd.to_numeric(df.loc[:, 'st_time'])
-    df.loc[:, 'ed_time'] = pd.to_numeric(df.loc[:, 'ed_time'])
-    df = df.drop_duplicates(keep='first')
+      
+      left = left.append(pd.Series([uId, sT, eT, tP, aJ, oT, ch, par], index=dfcols),ignore_index=True)
+      li.append(left)
+            
+  df = pd.concat(li, axis=0, ignore_index=True)
+  df.loc[:, 'st_time'] = pd.to_numeric(df.loc[:, 'st_time'])
+  df.loc[:, 'ed_time'] = pd.to_numeric(df.loc[:, 'ed_time'])
+  df = df.drop_duplicates(keep='first')
 
-    return df
+  return df
 
 def addDAoIVariable(df):
   # Add the 'Interruption' (bool) variable
@@ -123,17 +122,17 @@ def getLabels(segments, df_interruptions):
   '''
   input: segments: a list of segment file names, df_interruptions['meeting_id','st_time','ed_time', 'daoi']
   '''
-    counts = np.empty(segments.shape[0])
+  counts = np.empty(segments.shape[0])
 
-    for seg_index, seg_row in segments.iterrows():
-      for inter_index, inter_row in interruptions.iterrows():
-        if seg_row['meeting_id'] != inter_row['meeting_id']:
-          continue
-        elif seg_row['st_time'] < inter_row['st_time'] and seg_row['ed_time'] > inter_row['ed_time']:
-          counts[seg_index] += 1
-        else:
-          counts[seg_index] = 0
+  for seg_index, seg_row in segments.iterrows():
+    for inter_index, inter_row in interruptions.iterrows():
+      if seg_row['meeting_id'] != inter_row['meeting_id']:
+        continue
+      elif seg_row['st_time'] < inter_row['st_time'] and seg_row['ed_time'] > inter_row['ed_time']:
+        counts[seg_index] += 1
+      else:
+        counts[seg_index] = 0
 
-    # label as True if there's at least one entire interruption in the segment
-    labels = counts > 0 
-    return labels
+  # label as True if there's at least one entire interruption in the segment
+  labels = counts > 0 
+  return labels
