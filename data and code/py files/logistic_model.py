6ba9377eaf549a05fb0fc8098342f40c38c08948
Yangjingming Fu <yangjingming.fu@mail.mcgill.ca>
2022-06-15 10:00:08 -0400

    minor changes

--- data and code/py files/logistic_model.py
+++ data and code/py files/logistic_model.py
@@ -2,7 +2,7 @@ import torch
 from sklearn.linear_model import roc_curve
 
 class LogisticRegression(torch.nn.Module):
-     def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim):
          super(LogisticRegression, self).__init__()
          self.linear = torch.nn.Linear(input_dim, output_dim)     
          
